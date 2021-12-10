1df6aefaca08bd40a4741be9c2f6bbe6c9a2854b
Otto Manuel Garcia Preval <ottomanuelgarcia@gmail.com>
2021-12-10 12:03:12 -0500

    Initial Commit

---
+++ TransNovoa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransNovoa
+//
+//  Created by Otto Manuel Garcia Preval on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
